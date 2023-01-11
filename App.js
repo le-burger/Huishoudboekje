484cde57c44ce75116fa9ceff498653725a9e472
Leander Burger <leanderburger@hotmail.com>
2023-01-11 14:13:51 +0100

    Added navigation

--- App.js
+++ App.js
@@ -1,17 +1,108 @@
-import { StyleSheet } from 'react-native';
-import AlleUitgaven from './screens/AlleUitgaven';
+import { AntDesign, Ionicons } from "@expo/vector-icons";
+import { StyleSheet, Button } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import AlleUitgaven from "./screens/AlleUitgaven";
+import BeheerUitgaven from "./screens/BeheerUitgaven";
+import RecenteUitgaven from "./screens/RecenteUitgaven";
+import { Colors } from "./constants/Colors";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+  const BottomTabs = createBottomTabNavigator();
+
+  function UitgavenOverzicht() {
+    return (
+      <BottomTabs.Navigator
+        screenOptions={({ navigation }) => ({
+          headerStyle: { backgroundColor: Colors.Primary },
+          headerTintColor: Colors.Accent,
+          tabBarStyle: { backgroundColor: Colors.Primary },
+          tabBarActiveTintColor: Colors.Accent,
+          headerRight: ({ tintColor }) => (
+            <AntDesign
+              style={{ marginHorizontal: 10 }}
+              name="edit"
+              size={24}
+              color={tintColor}
+              onPress={() => {
+                navigation.navigate("BeheerUitgaven");
+              }}
+            />
+          ),
+        })}
+      >
+        <BottomTabs.Screen
+          name="RecenteUitgaven"
+          component={RecenteUitgaven}
+          options={{
+            title: "Recente Uitgaven",
+            tabBarLabel: "Recent",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="hourglass" size={size} color={color} />
+            ),
+          }}
+        />
+        <BottomTabs.Screen
+          name="AlleUitgaven"
+          component={AlleUitgaven}
+          options={{
+            title: "Alle Uitgaven",
+            tabBarLabel: "Totaal",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="calendar" size={size} color={color} />
+            ),
+          }}
+        />
+      </BottomTabs.Navigator>
+    );
+  }
   return (
-    <AlleUitgaven />
+    <>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: Colors.Background },
+            headerTintColor: "white",
+          }}
+        >
+          <Stack.Screen
+            name="UitgavenOverzicht"
+            component={UitgavenOverzicht}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="BeheerUitgaven"
+            component={BeheerUitgaven}
+            options={({ navigation }) => ({
+              presentation: "modal",
+              headerRight: ({ tintColor }) => (
+              <Ionicons
+                name="close"
+                size={36}
+                color={tintColor}
+                onPress={() => {
+                  navigation.goBack();
+                }}
+              />
+            ),
+            })}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: Colors.Background,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ constants/Colors.js
@@ -0,0 +1,7 @@
+export const Colors = {
+    Primary: '#264653',
+    Secondary: '#2a9d8f',
+    Background: '#e9c46a',
+    Accent: '#f4a261',
+    Error: '#e76f51',
+}
